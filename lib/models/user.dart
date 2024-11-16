c24a1beb595fa276b9747aa649509dae704f03ab
Isadora Vieira Ramos <isadorachb@gmail.com>
2024-11-16 15:55:49 -0300

    Alteração nos tipos de usuário

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,14 +1,18 @@
-class User{
+class User {
   final String name;
   final UserType type;
   final String telephone;
   final String address;
 
-  User({required this.name, required this.type, required this.telephone, required this.address});
+  User(
+      {required this.name,
+      required this.type,
+      required this.telephone,
+      required this.address});
 
-  getUserName(){
+  getUserName() {
     return name;
   }
 }
 
-enum UserType { passenger, pilot, admin }
+enum UserType { patient, chaperone, volunteer }
