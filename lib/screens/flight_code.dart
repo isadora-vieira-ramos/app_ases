70e48ae6fe8f4237f1c46e3021c90104705bd722
Isadora Ramos <isadorachb@gmail.com>
2024-11-22 12:32:49 -0300

    Redimensionamento da tela quando o teclado abre

--- lib/screens/flight_code.dart
+++ lib/screens/flight_code.dart
@@ -201,120 +201,124 @@ class _FlightCodeState extends State<FlightCode> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('lib/images/logo.png', width: 200, height: 150),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  "Acompanhamento de acionamento ASES",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.montserrat(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text("Selecione o tipo de usuário:",
-                    style: GoogleFonts.montserrat(
-                        fontSize: 15, fontWeight: FontWeight.normal)),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 10.0, right: 10.0, bottom: 2, top: 2),
-                  child: DropdownButton(
-                    underline: const SizedBox(),
-                    value: selectedValue,
-                    items: <String>['Paciente', 'Acompanhante', 'Voluntário']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        alignment: Alignment.centerLeft,
-                        child: Text(value,
-                            style: const TextStyle(color: Colors.grey)),
-                      );
-                    }).toList(),
-                    onChanged: (String? value) {
-                      setState(() {
-                        selectedValue = value!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text("Insira seu código de acesso:",
-                    style: GoogleFonts.montserrat(
-                        fontSize: 15, fontWeight: FontWeight.normal)),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 120, right: 120),
-                child: TextField(
-                  onChanged: (value) {
-                    setState(() {
-                      flightCode = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(20.0)),
-                      hintText: "Ex: 1245AQ6",
-                      hintStyle: const TextStyle(color: Colors.grey)),
-                ),
-              ),
-              if (errorMessage != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    errorMessage!,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.red, fontSize: 14),
-                  ),
-                ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10, top: 20),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Theme.of(context).colorScheme.secondary)),
-                  onPressed: () => validateAndSubmit(context),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('lib/images/logo.png', width: 200, height: 150),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
                     child: Text(
-                      "Entrar",
+                      "Acompanhamento de acionamento ASES",
+                      textAlign: TextAlign.center,
                       style: GoogleFonts.montserrat(
+                          fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text("Selecione o tipo de usuário:",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 15, fontWeight: FontWeight.normal)),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
                         color: Colors.white,
-                        fontSize: 18,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10.0, right: 10.0, bottom: 2, top: 2),
+                      child: DropdownButton(
+                        underline: const SizedBox(),
+                        value: selectedValue,
+                        items: <String>['Paciente', 'Acompanhante', 'Voluntário']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            alignment: Alignment.centerLeft,
+                            child: Text(value,
+                                style: const TextStyle(color: Colors.grey)),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            selectedValue = value!;
+                          });
+                        },
                       ),
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text("Insira seu código de acesso:",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 15, fontWeight: FontWeight.normal)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 120, right: 120),
+                    child: TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          flightCode = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20.0)),
+                          hintText: "Ex: 1245AQ6",
+                          hintStyle: const TextStyle(color: Colors.grey)),
+                    ),
+                  ),
+                  if (errorMessage != null)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        errorMessage!,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(color: Colors.red, fontSize: 14),
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10, top: 20),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Theme.of(context).colorScheme.secondary)),
+                      onPressed: () => validateAndSubmit(context),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          "Entrar",
+                          style: GoogleFonts.montserrat(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: showRecoveryMessage,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text('Esqueci meu código',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.montserrat(
+                            color: Colors.black,
+                            decoration: TextDecoration.underline,
+                          )),
+                    ),
+                  )
+                ],
               ),
-              GestureDetector(
-                onTap: showRecoveryMessage,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: Text('Esqueci meu código',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.montserrat(
-                        color: Colors.black,
-                        decoration: TextDecoration.underline,
-                      )),
-                ),
-              )
-            ],
+            ),
           ),
         ),
       ),
